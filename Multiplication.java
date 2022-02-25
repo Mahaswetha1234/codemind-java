759b07cb6b45914108ebd688e7b3e440a6d1bcec
swetha <69588423+Mahaswetha1234@users.noreply.github.com>
2022-02-25 22:37:45 +0530

    import java.util.*; class Multiply {     public static void main(String args[]){         Scanner s=new Scanner(System.in);         long a=s.nextLong();         long b=s.nextLong();         System.out.println(a*b);     } }

---
+++ Multiplication.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Multiply {
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        long a=s.nextLong();
+        long b=s.nextLong();
+        System.out.println(a*b);
+    }
+}
