98845f8e9438a32233d347826a1cd89a1478c5cd
swetha <69588423+Mahaswetha1234@users.noreply.github.com>
2022-02-25 23:00:18 +0530

    import java.util.*; class Print{     public static void main(String args[]){         Scanner s=new Scanner(System.in);         String a=s.nextLine();         System.out.println(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Print{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        String a=s.nextLine();
+        System.out.println(a);
+    }
+}
